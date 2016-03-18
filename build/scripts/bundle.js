82394d1642ec46f7230b9220325934f811fa5763
Murali Manohar <muralimano28@gmail.com>
2016-03-18 20:04:12 +0530

    Created a basic calendar component

--- build/scripts/bundle.js
+++ build/scripts/bundle.js
@@ -19035,9 +19035,52 @@ module.exports = require('./lib/React');
 
 var React = require("react");
 
+// Global variables
+var daysLabel = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    monthsLabel = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+    checkLeapYear = function (month, year) {
+        if (month == 1) { // February only!
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+                return true;
+            }
+        }
+        return false;
+    };
+
+
 var Calendar = React.createClass({displayName: "Calendar",
+    getInitialState: function () {
+        return ({
+            "currentDay":  null, // Stores day of the week (from 0 - 6)
+            "currentDate": null, // Stores day of the month (from 1 - 31)
+            "currentMonth": null, // Stores month (from 0 - 11)
+            "currentYear": null // Stores year
+        });
+    },
+
+    getDefaultProps: function () {
+        return ({
+            "defaultDate": new Date(),
+            "startDateLimit": new Date(),
+            "endDateLimit": null,
+            "onChange": function() {}
+        });
+    },
+
+    componentWillMount: function () {
+        // Storing states based on the input props.
+        this.setState({
+            "currentDay": this.props.defaultDate.getDay(),
+            "currentDate": this.props.defaultDate.getDate(),
+            "currentMonth": this.props.defaultDate.getMonth(),
+            "currentYear": this.props.defaultDate.getFullYear()
+        });
+    },
     render: function () {
-        return (React.createElement("section", null, React.createElement("h1", null, "I am calendar")));
+        return (React.createElement("section", null, 
+                    React.createElement("h1", null, "I am calendar")
+                ));
     }
 });
 
--- src/components/calendar.js
+++ src/components/calendar.js
@@ -2,9 +2,52 @@
 
 var React = require("react");
 
+// Global variables
+var daysLabel = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    monthsLabel = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+    checkLeapYear = function (month, year) {
+        if (month == 1) { // February only!
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+                return true;
+            }
+        }
+        return false;
+    };
+
+
 var Calendar = React.createClass({
+    getInitialState: function () {
+        return ({
+            "currentDay":  null, // Stores day of the week (from 0 - 6)
+            "currentDate": null, // Stores day of the month (from 1 - 31)
+            "currentMonth": null, // Stores month (from 0 - 11)
+            "currentYear": null // Stores year
+        });
+    },
+
+    getDefaultProps: function () {
+        return ({
+            "defaultDate": new Date(),
+            "startDateLimit": new Date(),
+            "endDateLimit": null,
+            "onChange": function() {}
+        });
+    },
+
+    componentWillMount: function () {
+        // Storing states based on the input props.
+        this.setState({
+            "currentDay": this.props.defaultDate.getDay(),
+            "currentDate": this.props.defaultDate.getDate(),
+            "currentMonth": this.props.defaultDate.getMonth(),
+            "currentYear": this.props.defaultDate.getFullYear()
+        });
+    },
     render: function () {
-        return (<section><h1>I am calendar</h1></section>);
+        return (<section>
+                    <h1>I am calendar</h1>
+                </section>);
     }
 });
 
